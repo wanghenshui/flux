7ee5d8b703727c6352bbeb70198633ba66705c5a
Tristan Brindle <t.c.brindle@gmail.com>
2024-11-14 18:21:15 +0000

    Silence MSVC warning in jtchkdint.h
    C4146 unary minus applied to unsigned type

--- include/flux/core/detail/jtckdint.h
+++ include/flux/core/detail/jtckdint.h
@@ -83,7 +83,7 @@
 
 #if defined(_MSC_VER)
 #pragma warning(push)
-#pragma warning(disable:4244)
+#pragma warning(disable: 4146 4244)
 #endif
 
 #if ((defined(__llvm__) ||                                              \
